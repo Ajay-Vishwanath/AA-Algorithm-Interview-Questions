d91e721ce2f0440f6abb0f3a4e20b6aa9122cf09
Ajay <ajayvishwanath.94@gmail.com>
2019-09-26 16:13:10 -0700

    Finish DepthBreadthFirst Questions

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,17 @@
 function breadthFirstArray(root) {
+    let result = []
+    let queue = [root];
 
+    while (queue.length){
+        let node = queue.shift();
+
+        result.push(node.val);
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    
+    return result;
 }
 
 module.exports = {
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,21 @@
 function treeSum(root) {
+    let sum = 0;
+    let queue = [root];
 
+    if (!root){
+        return 0
+    };
+
+    while (queue.length){
+        let node = queue.shift();
+
+        sum += node.val;
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return sum
 }
 
 
